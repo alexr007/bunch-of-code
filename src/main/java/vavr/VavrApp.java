d37422495161458a327f76c653c728c851b61807
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-05 13:34:58 +0400

    -- obsolete code removed -- pom.xml cleaned

--- src/main/java/vavr/VavrApp.java
+++ src/main/java/vavr/VavrApp.java
@@ -4,6 +4,8 @@ import io.vavr.collection.List;
 import io.vavr.control.Either;
 import io.vavr.control.Option;
 
+import java.util.function.Function;
+
 public class VavrApp {
   public Option<Integer> toInt(String origin) {
     try {
@@ -13,7 +15,15 @@ public class VavrApp {
     }
   }
 
-  public static void main(String[] args) {
+  public Either<String, Integer> toInt_(String origin) {
+    try {
+      return Either.right(Integer.parseInt(origin));
+    } catch (NumberFormatException ex) {
+      return Either.left("Conversion error");
+    }
+  }
+
+  public static void main1(String[] args) {
     List.of(1,2,3,4,5,6,7,8)
         .filter(x -> x < 5)
         .map(x -> x * 2)
@@ -22,6 +32,23 @@ public class VavrApp {
 
     VavrApp app = new VavrApp();
     Either<String, Integer> conversion_error = app.toInt("33").toEither("conversion error");
+  }
+
+  public static void main(String[] args) {
+    VavrApp app = new VavrApp();
+    Either<String, Integer> integers1 = app.toInt_("111");
+    Either<String, Integer> integers2 = app.toInt_("111ass");
+    String fold = integers1.fold(new Function<String, String>() {
+      @Override
+      public String apply(String s) {
+        return "Error:" + s;
+      }
+    }, new Function<Integer, String>() {
+      @Override
+      public String apply(Integer integer) {
+        return "OK:" + integer;
+      }
+    });
   }
 
 }
