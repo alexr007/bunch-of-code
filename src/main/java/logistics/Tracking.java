f4a373788faca2e456a6764bce9e20b054ff8b8e
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-21 11:01:03 +0400

    -- tracking parsing

--- src/main/java/logistics/Tracking.java
+++ src/main/java/logistics/Tracking.java
@@ -4,49 +4,34 @@ import logistics.extract.Match;
 import logistics.extract.Pt;
 import logistics.extract.Company;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Stream;
 
 public class Tracking {
 
-  static class Ex {
-    public final Function<String, Optional<String>> extractor;
-    public final Function<String, Company> create_company;
-
-    public Ex(Function<String, Optional<String>> extractor, Function<String, Company> create_company) {
-      this.extractor = extractor;
-      this.create_company = create_company;
-    }
-
-    public static Ex of(Function<String, Optional<String>> extractor, Function<String, Company> create) {
-      return new Ex(extractor, create);
-    }
-  }
-
-  private final static List<Ex> parsers = new ArrayList<Ex>(7) {{
-    add(Ex.of(s -> Match.many(s, Pt.TR_UPS),    Company::UPS));
-    add(Ex.of(s -> Match.many(s, Pt.TR_FEDEX),  Company::FedEx));
-    add(Ex.of(s -> Match.many(s, Pt.TR_USPS),   Company::USPS));
-    add(Ex.of(s -> Match.many(s, Pt.TR_DHL),    Company::DHL));
-    add(Ex.of(s -> Match.many(s, Pt.TR_AMAZON), Company::Amazon));
-    add(Ex.of(s -> Match.many(s, Pt.TR_AMT),    Company::AMT));
-    add(Ex.of(s -> Match.many(s, Pt.TR_CHINA),  Company::Noname));
-  }};
+  private final static List<Function<String, Optional<Company>>> parsers = Arrays.asList(
+      s -> Match.many(s, Pt.TR_UPS).map(Company::UPS),
+      s -> Match.many(s, Pt.TR_FEDEX).map(Company::FedEx),
+      s -> Match.many(s, Pt.TR_USPS).map(Company::USPS),
+      s -> Match.many(s, Pt.TR_DHL).map(Company::DHL),
+      s -> Match.many(s, Pt.TR_AMAZON).map(Company::Amazon),
+      s -> Match.many(s, Pt.TR_AMT).map(Company::AMT),
+      s -> Match.many(s, Pt.TR_CHINA).map(Company::Noname)
+  );
 
   /**
    * it parses given String to Company
    * if no company found - return Company.Unknown
    */
-  public static Company parse(String origin_) {
-    return Match.one(origin_, Pt.ANY_TRACK) // Optional<String>
-        .flatMap(origin -> parsers.stream()
-            .map(parser -> parser.extractor.apply(origin).map(parser.create_company))
-            .flatMap(c -> c.map(Stream::of).orElseGet(Stream::empty))
-            .findFirst())
-        .orElseGet(() -> Company.Unknown(origin_));
+  public static Company parse(String origin) {
+    return parsers.stream()
+        .map(f -> f.apply(origin))
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .findFirst()
+        .orElseGet(() -> Company.Unknown(origin));
   }
 
 }
