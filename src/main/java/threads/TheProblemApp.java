59d77710881d57035cff0f5f81a883ac64e52124
Alexey <alexey.rykhalskiy@gmail.com>
2020-03-11 14:31:28 +0400

    -- multithreading problem

---
+++ src/main/java/threads/TheProblemApp.java
@@ -0,0 +1,42 @@
+package threads;
+
+import lombok.SneakyThrows;
+
+public class TheProblemApp {
+
+  static class Box {
+    int value;
+  }
+
+  @SneakyThrows
+  public static void main(String[] args)  {
+
+    Box box = new Box();
+    box.value = 1;
+
+    {
+      new Thread(new Runnable() {
+        @SneakyThrows
+        @Override
+        public void run() {
+          Thread.sleep(500);
+          box.value = 42;
+        }
+      }).start();
+      new Thread(new Runnable() {
+        @SneakyThrows
+        @Override
+        public void run() {
+          Thread.sleep(1500);
+          box.value = 13;
+        }
+      }).start();
+    }
+
+    System.out.println(box.value);
+    Thread.sleep(1000);
+    System.out.println(box.value);
+    Thread.sleep(1000);
+    System.out.println(box.value);
+  }
+}
